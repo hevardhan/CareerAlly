fb14c44eb19dd9c32e7db33067370fe522b9b27e
hevardhan <paatukkorupulavan@gmail.com>
2024-11-09 15:42:38 +0530

    app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
 import About from './pages/About';
-import Test from './pages/Test';
 import Splash from './pages/Splash';
 import Chat from './pages/Chat';
 import Conversations from './pages/Conversations';
 import Connections from './pages/Connections';
 import Settings from './pages/Settings';
 import SignUp from './pages/SignUp';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Spline from './pages/spline';
 
 function App() {
   
@@ -20,13 +15,11 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
-        {/* <Route path="/" element={<Test />} /> */}
         <Route path="/" element={<Splash />} />
         <Route path="signup" element={<SignUp />} />
         <Route path="home" element={<Spline />} />
         <Route path='about' element={<About/>} />
         <Route path='chat' element={<Chat/>} />
-        {/* <Route path='spline' element={<Spline />} /> */}
         <Route path='convo' element={<Conversations/>} />
         <Route path='connect' element={<Connections/>} />
         <Route path='settings' element={<Settings/>} />
